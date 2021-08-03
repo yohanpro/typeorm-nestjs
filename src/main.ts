97dcd8350c8f633b59eca5d383d1eb6306888ee9
Yohan <yohan@yohanpro.com>
2021-08-03 09:32:24 +0900

    init commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 8080;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+
+  Logger.log(`Server is now runnig on ${port}`, 'Bootstarp');
+}
+bootstrap();
