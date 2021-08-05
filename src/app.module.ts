f212295d8080839304caaddcf8ad335c1044aee9
Yohan <yohan@yohanpro.com>
2021-08-05 23:52:13 +0900

    Feat: Idea 연습 모듈 삭제, 핫 리로딩 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { IdeaModule } from './idea/idea.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), IdeaModule],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 const port = process.env.PORT || 8080;
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 
   Logger.log(`Server is now runnig on ${port}`, 'Bootstarp');
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
